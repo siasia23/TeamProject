bb02b287d871b584fd59fa7ec4a174f884f3ab86
park-jinh <jinh.drive@gmail.com>
2024-01-31 09:09:18 +0900

    new branch 테스트 개발용

---
+++ test/src/main/java/com/oracle/test/controller/TestController.java
@@ -0,0 +1,12 @@
+package com.oracle.test.controller;
+
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class TestController {
+
+	public String testMethod() {
+		return "test";
+	}
+	
+}
---
+++ test/src/main/java/com/oracle/test/service/TestService.java
@@ -0,0 +1,9 @@
+package com.oracle.test.service;
+
+public class TestService {
+
+	public void serviceLogic() {
+		System.out.println("test 서비스 입니다.");
+	}
+	
+}
